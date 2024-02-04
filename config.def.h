ee5a56f2149a4649e7155fabc2b21df5ff426a90
entnuo <neptunemoon051@protonmail.com>
2024-02-03 23:53:53 -0300

    changed day taklajfaksfjaslkjglksa

--- config.def.h
+++ config.def.h
@@ -67,5 +67,5 @@ static const struct arg args[] = {
 	/* function format          argument */
 	//{ datetime, "%s ",           "%F %T" },
         { run_command, " %s ", "pactl list sinks|awk '$1==\"Volume:\" {print $5}'" },
-	{ run_command, "%s ", "LC_ALL=ja_JP.utf8 date +\"%Y年 %B %d日 %A %H:%M\"" },
+	{ run_command, "%s ", "LC_ALL=ja_JP.utf8 date +\"%Y年 %B %e日 %A %H:%M\"" },
 };
